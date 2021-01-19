0f69cf26ddc9275a3606579815858883aafa0d8c
Petr Kldov <petr.kladov@gmail.com>
2021-01-19 23:05:32 +0400

    fixed adding function, updated 404 page, paths taken out to dict

--- app/views.py
+++ app/views.py
@@ -8,6 +8,14 @@ import sqlite3
 import os
 
 
+path = {
+    'favicon': 'favicon.ico',
+    'index': '',
+    'add': 'add',
+    'delete': 'delete'
+}
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -16,9 +24,8 @@ def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('../schema.sql', 'r') as f:
             db.cursor().executescript(f.read())
-        db.execute('insert into entries (full, alias, password) values (?, ?, ?)', ('', '', ''))
-        db.execute('insert into entries (full, alias, password) values (?, ?, ?)', ('', 'add', ''))
-        db.execute('insert into entries (full, alias, password) values (?, ?, ?)', ('', 'delete', ''))
+        for key in path.keys():
+            db.execute('insert into entries (full, alias, password) values (?, ?, ?)', ('', path[key], ''))
         db.commit()
 
 
@@ -63,15 +70,18 @@ def direct(alias):
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found():
     context = dict()
     context['pagename'] = 'Ошибка 404'
     return render_template('404.html', context=context), 404
 
 
 def check_alias(alias):
-    cur = g.db.execute('select id from entries where alias = (?)', (alias,))
-    return not cur.fetchall()
+    cur = g.db.execute('select expiration from entries where alias = (?)', (alias,))
+    exp = cur.fetchall()
+    if not exp:
+        return False
+    return exp[0][0]
 
 
 def check_alias_sym(alias):
@@ -79,37 +89,43 @@ def check_alias_sym(alias):
     for sym in alias:
         if not sym.isalnum():
             if sym not in perm:
-                return False
-    return True
+                return True
+    return False
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/' + path['index'], methods=['GET', 'POST'])
+@app.route('/' + path['add'], methods=['GET', 'POST'])
 def add():
     form = AddForm()
     context = dict()
     context['pagename'] = 'Создание новой ссылки'
     context['form'] = form
     if form.validate_on_submit():
-        alias = form.shorten_path.data
+        alias = form.shorten_path.data.strip()
         if alias == '':
             cur = g.db.execute('select seq from sqlite_sequence')
-            id = cur.fetchall()[0][0]
-            while not check_alias(alias):
+            id = cur.fetchone()[0]
+            while True:
                 id += 1
                 hashid = Hashids(form.full.data)
                 alias = hashid.encode(id)
-            form.shorten_path.data = alias
+                check = check_alias(alias)
+                if not check and check is not None:
+                    break
         else:
-            alias = alias.strip()
             if len(alias) > 2048 - len('http://127.0.0.1:5000/'):
                 flash('Использовано слишком много символов, введите другой псевдоним', 'warning')
                 return render_template('add.html', context=context)
-            if not check_alias_sym(alias):
+            if check_alias_sym(alias):
                 flash('Использованны запрещенные символы, введите другой псевдоним', 'warning')
                 return render_template('add.html', context=context)
-            if not check_alias(alias):
-                flash('Такой псевдоним уже существует, введите другой', 'warning')
+            exp = check_alias(alias)
+            if isinstance(exp, int) and not isinstance(exp, bool):
+                flash('Такой псевдоним уже существует, срок действия закончится через {}ч., '
+                      'введите другой'.format(int((exp - int(time()))/3600) + 1), 'warning')
+                return render_template('add.html', context=context)
+            if exp is None:
+                flash('Такой псевдоним уже существует, срок действия неограничен, введите другой', 'warning')
                 return render_template('add.html', context=context)
         g.db.execute('insert into entries (full, alias, password, expiration) values (?, ?, ?, ?)',
                      (form.full.data, alias, form.password.data, int(time()) + 86400))
@@ -118,13 +134,16 @@ def add():
     return render_template('add.html', context=context)
 
 
-@app.route('/delete', methods=['GET', 'POST'])
+@app.route('/' + path['delete'], methods=['GET', 'POST'])
 def delete():
     form = DeleteForm()
     context = dict()
     context['pagename'] = 'Удаление ссылки'
     context['form'] = form
     if form.validate_on_submit():
+        if form.shorten_path.data in path.values():
+            flash('Нельзя удалить ссылку, необходимую для работы сайта', 'danger')
+            return render_template('delete.html', context=context)
         cur = g.db.execute('select password from entries where alias = (?)', (form.shorten_path.data,))
         try:
             password = str(cur.fetchall()[0][0])
@@ -135,13 +154,16 @@ def delete():
                 g.db.execute('update sqlite_sequence set seq = (?)', (ids,))
                 g.db.commit()
                 flash('Успешное удаление', 'success')
+                return render_template('delete.html', context=context)
             else:
                 flash('Неверный пароль', 'danger')
+                return render_template('delete.html', context=context)
         except IndexError:
             flash('Несуществующая ссылка', 'danger')
+            return render_template('delete.html', context=context)
     return render_template('delete.html', context=context)
 
 
-@app.route('/favicon.ico')
+@app.route('/' + path['favicon'])
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
