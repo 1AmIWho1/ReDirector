b5c798d1ce1370856d1b17d1ebeb6f86014b7368
Petr Kldov <petr.kladov@gmail.com>
2021-01-22 16:09:13 +0400

    fixed database initialization for Linux

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-DATABASE = basedir + '/tmp/data.db'
+DATABASE = 'tmp\\data.db'
 DEBUG = True
