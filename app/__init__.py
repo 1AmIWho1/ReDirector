bef30deb9f678db3b4a7cdb52eba3c6bb5b9dc50
Petr Kladov <petr.kladov@gmail.com>
2021-08-30 16:22:36 +0300

    Fixed creating database, deleted unnecessary code

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,9 @@ app.config.from_object('config')
 from app import views
 
 
-if platform.system() == 'Linux':
-    if not os.path.exists(os.getcwd() + '/' + app.config['DATABASE']):
-        views.init_db()
-elif platform.system() == 'Windows':
-    if not os.path.exists(os.getcwd() + '\\' + app.config['DATABASE'].replace('/', '\\')):
-        views.init_db()
-else:
+if not os.path.exists(app.config['DATABASE']):
     views.init_db()
+
 
 sched = BackgroundScheduler(timezone='Europe/Moscow')
 sched.add_job(views.my_scheduled_job, 'interval', hours=1)
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
-DATABASE = 'tmp/data.db'
+DATABASE = './tmp/data.db'
 DEBUG = True
